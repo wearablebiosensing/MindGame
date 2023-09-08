fe193e0be9f8bb10ae122d2f9b46573b677cd979
elijahcastillo <104207042+elijahcastillo@users.noreply.github.com>
2023-09-07 23:08:32 -0400

    Add Mouse data to the right to the screen

--- CareWear-MagneticTilesActivity/static/js/mouse_movement.js
+++ CareWear-MagneticTilesActivity/static/js/mouse_movement.js
@@ -23,20 +23,18 @@ setInterval(function () {
     var movementY = Math.abs(y - prev_y);
     var movement = Math.sqrt(movementX * movementX + movementY * movementY);
 
-    document.getElementById("currentWindowX").innerText = currentEvent.clientX;
-    document.getElementById("currentWindowY").innerText = currentEvent.clientY;
-
-    document.getElementById("movementX").innerText = movementX;
-    document.getElementById("movementY").innerText = movementY;
-    document.getElementById("movement").innerText = Math.round(movement);
+    document.getElementById("mouse_analytics_xpos").innerText =
+      currentEvent.clientX;
+    document.getElementById("mouse_analytics_ypos").innerText =
+      currentEvent.clientY;
 
     //speed=movement/100ms= movement/0.1s= 10*movement/s
     var speed = 10 * movement; //current speed
 
-    document.getElementById("speed").innerText = Math.round(speed);
-    document.getElementById("maxSpeed").innerText = Math.round(
-      speed > maxSpeed ? (maxSpeed = speed) : maxSpeed
-    );
+    // document.getElementById("speed").innerText = Math.round(speed);
+    // document.getElementById("maxSpeed").innerText = Math.round(
+    //   speed > maxSpeed ? (maxSpeed = speed) : maxSpeed
+    // );
 
     var acceleration = 10 * (speed - prevSpeed);
 
@@ -59,10 +57,10 @@ setInterval(function () {
     pMouseY = y;
     pTimestamp = currentTime;
 
-    document.getElementById("accelerationX").innerText = Math.round(
+    document.getElementById("mouse_analytics_xacc").innerText = Math.round(
       accelerationX_in_px_per_s_squared
     );
-    document.getElementById("accelerationY").innerText = Math.round(
+    document.getElementById("mouse_analytics_yacc").innerText = Math.round(
       accelerationY_in_px_per_s_squared
     );
 
